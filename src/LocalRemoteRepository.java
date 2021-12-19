dd1b9cbc20439a2265b7d53b4b8f3a22c67c6ea5
Inderjit57 <singhinderjit57@yahoo.com>
2021-12-19 00:51:59 -0500

    FirstLocalRepo

--- src/LocalRemoteRepository.java
+++ src/LocalRemoteRepository.java
@@ -3,7 +3,7 @@ public class LocalRemoteRepository {
 
 	public static void main(String[] args) {
 		int weatherDegree1 = 30;
-		int weatherDegree2 = 50;
+		int weatherDegree2 = 20;
 		float sum = weatherDegree1 + weatherDegree2;
 		
 		String weatherCondition = "Merry Christmas and Happy new year";
@@ -11,10 +11,10 @@ public class LocalRemoteRepository {
 
 		if (weatherDegree1 == weatherDegree2) 
 		{
-			System.out.println("its raining today: " + " " + weatherDegree1 + unit);
+			System.out.println("its raining today: "+" "+ weatherDegree1 + unit);
 		} else if (weatherDegree1 > weatherDegree2)
 		{
-			System.out.println("its not raining: " + " " + weatherDegree2 + unit);
+			System.out.println("its not raining: "+" "+ weatherDegree2 + unit);
 		}
 		else
 		{
